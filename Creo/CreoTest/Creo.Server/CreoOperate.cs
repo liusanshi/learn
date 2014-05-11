342b0aeaf08a74c6a03dfb02e833045cb88848fc
sanshi <ll_nh@163.com>
2014-05-11 22:28:30 +0800

    项目进度第二天
    项目进度第二天

--- Creo/CreoTest/Creo.Server/CreoOperate.cs
+++ Creo/CreoTest/Creo.Server/CreoOperate.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 
 using Proway.PLM.Document;
 using Proway.Framework;
@@ -23,11 +24,17 @@ namespace Creo.Server
         {
             base.ImportValidator(server);
             //验证删除
-            server.RegisterValidator(new ValidateDeleteDoc(this.dicDeleteDoc, (DocumentVersion docver, DocStruct doc) => BOMHelp.IsEquals(BOMHelp.GetFileNameNoVersion(docver.FileName), BOMHelp.GetFileNameNoVersion(doc.FileName))));
+            //server.RegisterValidator(new ValidateDeleteDoc(this.dicDeleteDoc, (DocumentVersion docver, DocStruct doc) => BOMHelp.IsEquals(BOMHelp.GetFileNameNoVersion(docver.FileName), BOMHelp.GetFileNameNoVersion(doc.FileName))));
             //验证删除参数
-            server.RegisterValidator(new ValidateDocConfig(base.IndexFields));
+            //server.RegisterValidator(new ValidateDocConfig(base.IndexFields));
             //验证物料是否已经被使用
-            server.RegisterValidator(new ValidateCanUseMaterial());
+            //server.RegisterValidator(new ValidateCanUseMaterial());
+
+            var empty = new ValidateEmpty();
+            server.Replace(typeof(ValidateUpdateAddin), empty);
+            server.Replace(typeof(ValidateStandardParts), empty);
+            server.Replace(typeof(ValidateStandardPartsState), empty);
+            server.Replace(typeof(ValidateStandardPartsMaterialRepeat), empty);
         }
 
         /// <summary>
@@ -37,13 +44,62 @@ namespace Creo.Server
         protected override void CheckInValidator(ValidateServer server)
         {
             base.CheckInValidator(server);
-            Type typeFromHandle = typeof(ValidateIsNewMaterial);
+            
             //验证删除参数
-            server.InsertBefore(typeFromHandle, new ValidateDocConfig(base.IndexFields));
+            //server.InsertBefore(typeFromHandle, new ValidateDocConfig(base.IndexFields));
             //是否有删除配置
-            server.InsertBefore(typeFromHandle, new ValidateDocConfigDelete());
+            //server.InsertBefore(typeFromHandle, new ValidateDocConfigDelete());
             //验证物料是否已经被使用
-            server.RegisterValidator(new ValidateCanUseMaterial());
+            //server.RegisterValidator(new ValidateCanUseMaterial());
+            var empty = new ValidateEmpty();
+            server.Replace(typeof(ValidateUpdateAddin), empty);
+            server.Replace(typeof(ValidateStandardParts), empty);
+            server.Replace(typeof(ValidateStandardPartsState), empty);
+            server.Replace(typeof(ValidateStandardPartsMaterialRepeat), empty);
+            server.Replace(typeof(ValidateStandardPartsDelete), empty);
+        }
+
+        protected override BOMStruct MergeDcounentByProperty(BOMStruct bom)
+        {
+            bool flag = bom.IsolatedNodes.Any<DocStruct>();
+
+            TraceWriteLine("flag :" + flag);
+            foreach (DocStruct current in bom.FindRootConfigs())
+            {
+                TraceWriteLine("进入循环");
+                if (!current.IsConfigPart)
+                {
+                    break;
+                }
+                TraceWriteLine("进入循环 里面的处理");
+                current.AddValue("__isrootconfig", true);
+                current.AddValue("__hasolatedconfig", flag);
+                if (current.ConfigName == current.GetString("activeconfig"))
+                {
+                    current.IsActive = true;
+                    foreach (DocStruct current2 in current.GetDescendant())
+                    {
+                        current2.IsActive = true;
+                    }
+                }
+            }
+            foreach (DocView current3 in bom.BOMView)
+            {
+                current3.GetCurrentData().WriteValue("__defaultdoc", true);
+            }
+            return base.MergeDcounentByProperty(bom);
+        }
+
+        /// <summary>
+        /// 写日志
+        /// </summary>
+        /// <param name="msg"></param>
+        [Conditional("DEBUG")]
+        void TraceWriteLine(string msg)
+        {
+#if DEBUG
+      Proway.Framework.Core.Log.Info(msg);
+#endif
         }
     }
 }
---
+++ Creo/CreoTest/Creo.Server/ValidateEmpty.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Proway.Framework;
+
+namespace Creo.Server
+{
+    /// <summary>
+    /// 空的验证器，用于删除验证器
+    /// </summary>
+    public class ValidateEmpty : DefaultValidator
+    {
+        public override bool Validate(ValidateContext context)
+        {
+            return true;
+        }
+    }
+}
