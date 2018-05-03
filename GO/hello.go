0a1e8b6bfa6095373d50d16f8e83c6feabee52bc
payneliu(刘磊) <payneliu@tencent.com>
2018-05-03 19:50:16 +0800

    更改代码结构

--- GO/hello.go
+++ GO/hello.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"os"
 	"fmt"
-	"time"
 	"./test"
 )
 //import "unsafe"
@@ -28,34 +26,5 @@ func main(){
 	fmt.Println("Hello, World!");
 
 	test.Array()
-
-	ch := make(chan int, 100);
-
-	go func(){
-		for i := 0; i < 10; i++ {
-			ch <- i;
-			// println(time.Second)
-			time.Sleep(time.Second);
-		}
-
-		os.Exit(0)
-	}()
-
-	for{
-		select{
-			case n := <- ch:
-				println(n);
-			case <- timeAfter(time.Second*2):
-
-		}
-	}
 }
 
-func timeAfter(d time.Duration) chan int{
-	q := make(chan int, 1)
-	time.AfterFunc(d, func(){
-		q <- 1
-		println("run");
-	})
-	return q;
-}
